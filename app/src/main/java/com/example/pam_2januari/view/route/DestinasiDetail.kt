12df250a7769a0c0a69f2ae92eae5194a9e4f03d
Ajopratama1 <a.ajopratama01@gmail.com>
2026-01-06 16:40:39 +0700

    Menambahkan rute navigasi untuk detail siswa
    Membuat object `DestinasiDetail` baru yang mengimplementasikan `DestinasiNavigasi`. Ini mendefinisikan rute navigasi "detail_siswa", resource judul, dan argumen navigasi `idSiswa`.

---
+++ app/src/main/java/com/example/pam_2januari/view/route/DestinasiDetail.kt
@@ -0,0 +1,10 @@
+package com.example.pam_2januari.view.route
+
+import com.example.pam_2januari.R
+
+object DestinasiDetail: DestinasiNavigasi {
+    override val route = "detail_siswa"
+    override val titleRes = R.string.detail_siswa
+    const val itemIdArg = "idSiswa"
+    val routeWithArgs = "$route/{$itemIdArg}"
+}
