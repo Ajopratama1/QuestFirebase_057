f8e0f891e2ccc58f9454d938a6d8c70de822c3f0
Ajopratama1 <a.ajopratama01@gmail.com>
2026-01-06 16:41:34 +0700

    Menambahkan objek navigasi DestinasiHome
    Menambahkan file `DestinasiHome.kt` baru yang mengimplementasikan `DestinasiNavigasi`. Objek ini mendefinisikan rute "home" dan judul aplikasi untuk tujuan navigasi utama.

---
+++ app/src/main/java/com/example/pam_2januari/view/route/DestinasiHome.kt
@@ -0,0 +1,8 @@
+package com.example.pam_2januari.view.route
+
+import com.example.pam_2januari.R
+
+object DestinasiHome : DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = R.string.app_name
+}
