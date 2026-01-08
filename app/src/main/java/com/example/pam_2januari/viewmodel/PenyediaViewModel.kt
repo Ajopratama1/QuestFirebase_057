182eabbe0ae0fb3d67bb4d776303085f56c74fc4
Ajopratama1 <a.ajopratama01@gmail.com>
2026-01-09 00:23:06 +0700

    Menambahkan initializer DetailViewModel dan EditViewModel ke PenyediaViewModel
    Memperbarui `PenyediaViewModel.kt` dengan:
    - Menambahkan initializer untuk `DetailViewModel` menggunakan `SavedStateHandle`.
    - Menambahkan initializer untuk `EditViewModel` menggunakan `SavedStateHandle`.
    - Menambahkan import `androidx.lifecycle.createSavedStateHandle`.

--- app/src/main/java/com/example/pam_2januari/viewmodel/PenyediaViewModel.kt
+++ app/src/main/java/com/example/pam_2januari/viewmodel/PenyediaViewModel.kt
@@ -1,6 +1,7 @@
 package com.example.pam_2januari.viewmodel
 
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
@@ -15,5 +16,9 @@ object PenyediaViewModel {
     val Factory = viewModelFactory {
         initializer { HomeViewModel(aplikasiDataSiswa().container.repositorySiswa) }
         initializer { EntryViewModel(aplikasiDataSiswa().container.repositorySiswa) }
+        initializer { DetailViewModel(this.createSavedStateHandle(),
+            aplikasiDataSiswa().container.repositorySiswa) }
+        initializer { EditViewModel(this.createSavedStateHandle(),
+            aplikasiDataSiswa().container.repositorySiswa) }
     }
 }
