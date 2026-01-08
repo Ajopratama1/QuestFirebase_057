3160e90ed6cb1827037d05adc2631150cd6deaf1
Ajopratama1 <a.ajopratama01@gmail.com>
2026-01-09 00:17:44 +0700

    Menambahkan EditViewModel untuk fitur edit siswa
    Menambahkan file `EditViewModel.kt` yang berfungsi untuk:
    - Mengelola state UI untuk form edit siswa menggunakan `UIStateSiswa`.
    - Mengambil ID siswa dari `SavedStateHandle` berdasarkan argumen navigasi.
    - Melakukan inisialisasi data siswa dari `RepositorySiswa` saat ViewModel dibuat.

---
+++ app/src/main/java/com/example/pam_2januari/viewmodel/EditViewModel.kt
@@ -0,0 +1,30 @@
+package com.example.pam_2januari.viewmodel
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.pam_2januari.modeldata.DetailSiswa
+import com.example.pam_2januari.modeldata.UIStateSiswa
+import com.example.pam_2januari.modeldata.toDataSiswa
+import com.example.pam_2januari.modeldata.toUiStateSiswa
+import com.example.pam_2januari.repositori.RepositorySiswa
+import com.example.pam_2januari.view.route.DestinasiDetail
+import kotlinx.coroutines.launch
+
+class EditViewModel(savedStateHandle: SavedStateHandle,private val repositorySiswa:
+RepositorySiswa): ViewModel() {
+    var uiStateSiswa by mutableStateOf(UIStateSiswa())
+        private set
+
+    private val idSiswa: Long =
+        savedStateHandle.get<String>(DestinasiDetail.itemIdArg)?.toLong()
+            ?: error("idSiswa tidak ditemukan di SavedStateHandle")
+    init {
+        viewModelScope.launch {
+            uiStateSiswa = repositorySiswa.getSatuSiswa(idSiswa)!!
+                .toUiStateSiswa(true)
+        }
+    }
