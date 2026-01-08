1d47e4b6191b047734f58816a16f0ab987fd6389
Ajopratama1 <a.ajopratama01@gmail.com>
2026-01-09 00:15:21 +0700

    Menambahkan ViewModel dan status UI untuk fitur detail siswa
    Menambahkan file `DetailViewModel.kt` yang berisi:
    - Definisi sealed interface `StatusUIDetail` untuk mengelola status UI (Success, Error, Loading).
    - Persiapan struktur `DetailViewModel` untuk mengambil dan mengelola data detail siswa.

---
+++ app/src/main/java/com/example/pam_2januari/viewmodel/DetailViewModel.kt
@@ -0,0 +1,22 @@
+@file:OptIn(InternalSerializationApi::class)
+
+package com.example.pam_2januari.viewmodel
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.pam_2januari.modeldata.Siswa
+import com.example.pam_2januari.repositori.RepositorySiswa
+import com.example.pam_2januari.view.route.DestinasiDetail
+import kotlinx.coroutines.launch
+import kotlinx.serialization.InternalSerializationApi
+import java.io.IOException
+
+sealed interface StatusUIDetail {
+    data class Success(val satusiswa: Siswa?) : StatusUIDetail
+    object Error : StatusUIDetail
+    object Loading : StatusUIDetail
+}
