8df89fc8911d68f9f26d43152383ecda2fc2a7ee
Ajopratama1 <a.ajopratama01@gmail.com>
2026-01-09 00:18:13 +0700

    Menambahkan fungsi validasi dan update UI state pada EditViewModel
    Memperbarui `EditViewModel.kt` dengan:
    - Menambahkan fungsi `updateUiState` untuk memperbarui state siswa dan menjalankan validasi.
    - Menambahkan fungsi private `validasiInput` untuk memastikan field nama, alamat, dan telepon tidak kosong.

--- app/src/main/java/com/example/pam_2januari/viewmodel/EditViewModel.kt
+++ app/src/main/java/com/example/pam_2januari/viewmodel/EditViewModel.kt
@@ -28,3 +28,14 @@ RepositorySiswa): ViewModel() {
                 .toUiStateSiswa(true)
         }
     }
+
+    fun updateUiState(detailSiswa: DetailSiswa) {
+        uiStateSiswa =
+            UIStateSiswa(detailSiswa = detailSiswa, isEntryValid = validasiInput
+                (detailSiswa))
+    }
+    private fun validasiInput(uiState: DetailSiswa = uiStateSiswa.detailSiswa ): Boolean {
+        return with(uiState) {
+            nama.isNotBlank() && alamat.isNotBlank() && telpon.isNotBlank()
+        }
+    }
