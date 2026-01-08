6d04503da41a52f9b3612c716eb944edbb3d0367
Ajopratama1 <a.ajopratama01@gmail.com>
2026-01-09 00:23:17 +0700

    Menambahkan rute navigasi untuk Detail dan Edit Siswa
    Memperbarui `PetaNavigasi.kt` dengan menambahkan konfigurasi composable baru untuk:
    - Layar Detail Siswa (`DestinasiDetail`): Mendukung argumen ID siswa dan navigasi ke halaman edit atau kembali ke home.
    - Layar Edit Siswa (`DestinasiEdit`): Mendukung argumen ID siswa dan navigasi kembali ke home atau ke atas (navigate up).

--- app/src/main/java/com/example/pam_2januari/view/controllNavigasi/PetaNavigasi.kt
+++ app/src/main/java/com/example/pam_2januari/view/controllNavigasi/PetaNavigasi.kt
@@ -8,9 +8,12 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
+import com.example.pam_2januari.view.DetailSiswaScreen
+import com.example.pam_2januari.view.EditSiswaScreen
 import com.example.pam_2januari.view.EntrySiswaScreen
 import com.example.pam_2januari.view.HomeScreen
 import com.example.pam_2januari.view.route.DestinasiDetail
+import com.example.pam_2januari.view.route.DestinasiEdit
 import com.example.pam_2januari.view.route.DestinasiEntry
 import com.example.pam_2januari.view.route.DestinasiHome
 
@@ -36,6 +39,20 @@ fun HostNavigasi(
         composable(DestinasiEntry.route){
             EntrySiswaScreen(navigateBack = { navController.navigate(DestinasiHome.route)
             })
+        }
+        composable(DestinasiDetail.routeWithArgs,arguments = listOf(navArgument
+            (DestinasiDetail.itemIdArg) {
+            type = NavType.StringType })
+        ){
+            DetailSiswaScreen(navigateToEditItem = {navController.navigate("${DestinasiEdit
+                .route}/$it")},
+                navigateBack = { navController.navigate(DestinasiHome.route) })
+        }
+        composable(DestinasiEdit.routeWithArgs, arguments = listOf(navArgument
+            (DestinasiEdit.itemIdArg){
+            type = NavType.StringType })){
+            EditSiswaScreen(navigateBack = { navController.navigate(DestinasiHome.route) },
+                onNavigateUp = { navController.navigateUp() })
         }
     }
 }
